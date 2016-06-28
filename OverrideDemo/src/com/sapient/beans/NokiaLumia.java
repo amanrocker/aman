65289303053ca0e275b8459b781fc377a4557ebd
asi280 <asi280@NOILAB66161.sapient.com>
2016-06-28 14:09:46 +0530

    aman committed override ex

---
+++ OverrideDemo/src/com/sapient/beans/NokiaLumia.java
@@ -0,0 +1,19 @@
+package com.sapient.beans;
+
+public class NokiaLumia extends Nokia1100{
+
+	public void doConverse(){
+		System.out.println("3g coverse");
+		
+	}
+	public void captureVideo(){
+		System.out.println("capture video");
+		
+	}
+	public void do2gconverse()
+	{
+		super.doConverse();
+		
+	}
+}
+
